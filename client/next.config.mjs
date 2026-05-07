bbb188b39025d4c1fd4a1eac76f5fd96c558d8ba
atharva-4 <atharvadeore04@gmail.com>
2026-05-08 03:09:04 +0530

    patch: deployment

--- client/next.config.mjs
+++ client/next.config.mjs
@@ -3,10 +3,11 @@ const nextConfig = {
   /* config options here */
   reactCompiler: true,
   async rewrites() {
+    const serverUrl = process.env.NEXT_PUBLIC_SERVER_URL || "http://localhost:3001";
     return [
       {
         source: "/api/:path*",
-        destination: `${process.env.NEXT_PUBLIC_SERVER_URL}/api/:path*`,
+        destination: `${serverUrl}/api/:path*`,
       },
     ];
   },
